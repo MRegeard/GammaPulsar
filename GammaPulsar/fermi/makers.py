4ec9c6ee7c86ef88b193c1a6d1cf5815f4f1c12c
Maxime Regeard <regeard@apc.in2p3.fr>
2023-06-02 14:44:10 +0200

    docstring and some clean up
    Signed-off-by: Maxime Regeard <regeard@apc.in2p3.fr>

--- GammaPulsar/fermi/makers.py
+++ GammaPulsar/fermi/makers.py
@@ -13,7 +13,27 @@ __all__ = ["FermiPhaseMaker"]
 
 
 class FermiPhaseMaker:
-    """ """
+    """
+    Class that compute of the pulsar phase of Fermi-LAT data.
+
+    Once the pulsar phase is computed, it can be written in the original Fermi-LAT events file or in a copy
+    of the original Fermi-lAT phase in a new column named "PULSE_PHASE" by default.
+
+    Parameters
+    ----------
+    observation : `GammaPulsar.fermi.FermiObservation`
+        The Fermi-LAT observation (events file and spacecraft file) to use.
+    ephemeris_file : str
+        Path to the ephemeris file to use for the pulsar phase computation
+    ephem : str
+        The name of the solar system ephemeris to use; defaults to "DE421".
+    include_bipm : bool or None
+        Whether to apply the BIPM clock correction. Defaults to False.
+    include_gps : bool or None
+        Whether to include the GPS clock correction. Defaults to False.
+    planets : boor or None
+        Whether to apply Shapiro delays based on planet positions.
+    """
 
     def __init__(
         self,
@@ -37,6 +57,14 @@ class FermiPhaseMaker:
         self.phases = None
 
     def compute_phase(self, **kwargs):
+        """
+        Compute the pulsar phase.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            Keyword arguments to pass to `pint.fermi_toas.load_Fermi_TOAs` .
+        """
 
         get_satellite_observatory(
             "Fermi", self.observation.fermi_spacecraft, overwrite=True
@@ -55,15 +83,26 @@ class FermiPhaseMaker:
         phases = self.model.phase(toas=ts, abs_phase=True)[1]
         self.phases = np.where(phases < 0.0, phases + 1.0, phases)
 
-    def open_file(self, mode):
-        hdulist = fits.open(self.observation.fermi_events.filename)
-        return hdulist
-
     def write_column_and_meta(
-        self, filename, column_name="PULSE_PHASE", overwrite=True
+        self, filename=None, column_name="PULSE_PHASE", overwrite=True
     ):
+        """
+        Write the pulsar phase as a new column in the original Fermi-LAT events file or in a copy of it.
+        Write some metadata related to the phase computation in the header of the original Fermi-LAT events
+        file or in a copy of it.
 
-        if filename == self.observation.fermi_events.filename:
+        Parameters
+        ----------
+        filename : str or None
+            The path to the filename to write the column phase to. Default is None.
+            If it is set to None, the column will be written in the original fermi-LAT events file.
+        column_name : str
+            The name of the column created to write the pulsar phase. Default is PULSE_PHASE, to match the
+            standard of Fermi-LAT analysis.
+        overwrite : bool
+            Whether to overwrite the column if a column with the same name already exist in the file.
+        """
+        if filename is None:
             hdulist = fits.open(self.observation.fermi_events.filename, mode="update")
         else:
             hdulist = fits.open(self.observation.fermi_events.filename)
@@ -89,7 +128,7 @@ class FermiPhaseMaker:
 
         hdulist[1] = bin_table
 
-        if filename == self.observation.fermi_events.filename:
+        if filename is None:
             hdulist.flush(verbose=True, output_verify="warn")
         else:
             hdulist.writeto(
@@ -98,6 +137,25 @@ class FermiPhaseMaker:
 
     @staticmethod
     def _make_meta(ephemeris_file, model, column_name="PULSE_PHASE", offset=None):
+        """
+        Make the metadata string to put in the header of the Fermi-LAT events file.
+
+        Parameters
+        ----------
+        ephemeris_file : str
+            Path to the ephemeris file that was used for the pulsar phase computation.
+        model : `pint.models.Model` ### TO CHECK ###
+            The PINT model for the epheremis file.
+        column_name : str
+            The name of the column on which the pulsar phase has been written. Default is PULSE_PHASE.
+        offset : float or None
+            The offset that was applied to the pulsar phase. Default is None.
+        Returns
+        -------
+        str(meta_dict) : str
+            The string of the dictionary that is build from the different metadata gather for the pulsar phase
+            computation?
+        """
         meta_dict = dict
         meta_dict["COLUMN_NAME"] = column_name
         meta_dict["EPHEMERIS_FILE"] = ephemeris_file
@@ -145,6 +203,24 @@ class FermiPhaseMaker:
 
     @staticmethod
     def _check_column_name(event_hdu, column_name, overwrite):
+        """
+        Check the if a column name named as the `column_name` parameter exist in the `event_hdu` , and whether
+        it is compatible with the overwrite parameter.
+
+        Parameters
+        ----------
+        event_hdu : `astropy.io.fits.hdu.table.BinTableHDU`
+            The FITS table to check for.
+        column_name : str
+            The name of the column to check.
+        overwrite : bool
+            Whether the column can be overwritten or not.
+
+        Returns
+        -------
+        check : bool
+            Whether the check passed.
+        """
         if column_name not in event_hdu.columns.names:
             log.info(f"Writing {column_name} to events file")
             return True
